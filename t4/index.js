f9edd3baa984c61bc68f729d9ddadb3b094abfb1
maryfar <maryam.farhadi.67@gmail.com>
2023-10-04 11:13:49 +0330

    Initial commit

---
+++ t4/index.js
@@ -0,0 +1,17 @@
+function showNotification(message, position, cssClass = '') {
+    const notification = document.createElement('div');
+    notification.textContent = message;
+    notification.classList.add('notification', position, cssClass);
+    document.body.appendChild(notification);
+  
+    setTimeout(() => {
+      notification.remove();
+    }, 3000);
+  }
+  
+  // Example usage:
+  const message = 'This is a notification message.';
+  const position = 'top-right';
+  const cssClass = 'custom-notification';
+  
+  showNotification(message, position, cssClass);
